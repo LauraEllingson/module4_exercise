a2d1d3f4c396c7eac2f9f835c601f3334324dc4f
LauraEllingson <lellingson@ufl.edu>
2024-09-14 17:22:28 -0400

    docker

--- schema/schema.sql
+++ schema/schema.sql
@@ -1,1 +1,6 @@
 -- Create your pizza database tables here!
+CREATE TABLE pizzas (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL
+);
+
--- schema/seed.sql
+++ schema/seed.sql
@@ -1,1 +1,5 @@
 -- Create seed data for your pizza database here
+INSERT INTO pizzas (name) VALUES ('Pepperoni');
+INSERT INTO pizzas (name) VALUES ('Everything');
+INSERT INTO pizzas (name) VALUES ('Hawaiian');
+
